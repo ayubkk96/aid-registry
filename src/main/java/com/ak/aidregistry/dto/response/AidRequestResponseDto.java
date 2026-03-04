f1698dbc8d0a4cd59f5bfc08783114d4e99d33b2
ayubkk96 <ayubkaou@gmail.com>
2026-03-04 21:54:12 +0000

    Added create request controller update, added aid request response dto, changed some invariant order

---
+++ src/main/java/com/ak/aidregistry/dto/response/AidRequestResponseDto.java
@@ -0,0 +1,10 @@
+package com.ak.aidregistry.dto.response;
+
+import com.ak.aidregistry.domain.AidRequestStatus;
+import com.ak.aidregistry.domain.ItemType;
+
+public class AidRequestResponseDto {
+
+    public AidRequestResponseDto(String id, ItemType itemType, int quantity, AidRequestStatus status) {
+    }
+}
